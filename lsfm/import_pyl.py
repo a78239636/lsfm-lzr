836aaa5f5a67b32f8d12e88585b306a121908c04
a78239636 <a78239636@163.com>
2020-11-25 09:57:20 +0800

    have add my landmark-file and import_pyl-file

---
+++ lsfm/import_pyl.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+from plyfile import PlyData,PlyProperty, PlyListProperty
+import numpy as np
+from lsfm import landmark_mesh, landmark_and_correspond_mesh
+from menpo.shape import ColouredTriMesh, TexturedTriMesh, TriMesh, PointCloud
+import lsfm.io as lio
+
+def headline(string):
+     print("\n\n----------{0}----------\n".format(string))
+
+def import_obj():
+     james =  Path('/home/li_gang/TestFile/NewInput2/james.obj')
+     man = Path('/home/li_gang/TestFile/NewInput2/man.obj')
+     obj_path = man
+     mesh = lio.import_mesh(obj_path)
+     print("mesh = ", mesh)
+     landmark_mesh(mesh)
+
+def import_full_ply(filename, verbose=True):
+     file_dir = str(filename) # 文件的路径
+     print("ply file name = ", file_dir)
+
+
+     from menpo3d.io.input.mesh.base import vtk_ensure_trilist
+     import vtk
+     ply_importer = vtk.vtkPLYReader()
+     ply_importer.SetFileName(str(file_dir))
+     ply_importer.Update()
+     polydata = ply_importer.GetOutput()
+     trilist = np.require(vtk_ensure_trilist(polydata), requirements=['C'])
+
+     plydata = PlyData.read(file_dir)
+     vertexs = plydata['vertex']
+
+     if (verbose is True):
+          headline("Meta Info of Ply File")
+          for element in plydata.elements:
+               print("Meta = {0}".format( element.name))
+          print(vertexs.dtype)
+          print("This is TriList : ", trilist)
+
+     points_list = []
+     colors_list = []
+     for verx in vertexs:
+         points_list.append( ( verx[0], verx[1], verx[2] ) )
+         colors_list.append( ( verx[3], verx[4], verx[5] ) )
+
+     nd_point = np.array(points_list, dtype=np.float64)
+     nd_color = np.array(colors_list, dtype=np.uint8)
+
+     if (verbose is True):
+          headline("Ndarray INFO")
+          print("Point shape = {0} \nColor shape = {1}".format(nd_point.shape, nd_color.shape))
+
+
+     mesh = ColouredTriMesh(nd_point, trilist=trilist, colours=nd_color)
+     if (verbose is True):
+          headline("Mesh INFO")
+          print(mesh)
+          print("color Shape =", mesh.colours.shape)
+          print("tri Shape = ",  mesh.trilist.shape)
+          print("tri List = ", mesh.trilist)
+     return mesh
+
+opt1 = '/home/li_gang/TestFile/HLSInput/face-reconstruction-template.ply'
+import_full_ply(opt1)
+#import_obj()
--- lsfm/landmark-changed.py
+++ lsfm/landmark-changed.py
@@ -1,5 +1,7 @@
 from functools import lru_cache
-from menpo.shape import PointCloud
+from pathlib import Path
+
+from menpo.shape import PointCloud, ColouredTriMesh
 from menpo.transform import Scale, Translation
 from menpo3d.rasterize import (
     rasterize_barycentric_coordinate_images,
@@ -10,6 +12,7 @@ from menpofit.aam import load_balanced_frontal_face_fitter
 
 from .camera import perspective_camera_for_template
 from .data import load_template, LANDMARK_MASK
+from .io import export_landmark_visualization
 
 
 @lru_cache()
@@ -29,8 +32,11 @@ def align_mesh_to_template(source, target, scale_corrective=1.2):
     return translation.compose_before(scale)
 
 
-def landmark_mesh(mesh, img_shape=(320, 240), verbose=False):
+def landmark_mesh_my(mesh, img_shape=(320, 240), verbose=False):
+#    print("Fist Mesh : {}, channel = {} ".format(mesh, mesh.n_channels))
+
     fitter = load_balanced_frontal_face_fitter()
+    # seemed a face detector
     detector = load_dlib_frontal_face_detector()
     camera = perspective_camera_for_template(img_shape)
 
@@ -38,13 +44,31 @@ def landmark_mesh(mesh, img_shape=(320, 240), verbose=False):
     aligned_mesh = align_mesh_to_template(mesh, load_template()).apply(mesh)
 
     mesh_in_img = camera.apply(aligned_mesh)
-
+    if (isinstance(mesh_in_img, ColouredTriMesh)):
+        print("--------Landmark Step ---------")
+        print("Mesh In Img Afrer Aligned = {0} \nchannel = {1} ".format(mesh_in_img, mesh_in_img.n_channels))
+        print(mesh_in_img.points)
+        print(mesh_in_img.colours)
+        print(mesh_in_img.trilist)
+    # bcs = (img1, img2) two mask image
     bcs = rasterize_barycentric_coordinate_images(mesh_in_img, img_shape)
+
+    print("bcoords_image type = ", bcs[0].as_vector())
+    print("tri_indices_image type = ", bcs[1].as_vector())
+   # print("bcs = ", bcs[0].as_vector())
+
+
+    # img : A rasterized image of the mesh.
     img = rasterize_mesh_from_barycentric_coordinate_images(mesh_in_img, *bcs)
     shape_img = rasterize_shape_image_from_barycentric_coordinate_images(
         mesh, *bcs)
+    print("!!!!!", type(img) )
 
-
+    print("Final Texture img ={0}  \nchannel = {1}".format(img, img.n_channels))
+  #  print("Final Shape img = ", shape_img)
+    print("Export Final image to : ", "/home/li_gang/TestFile/Inter-Imges")
+    print(img.as_vector())
+    export_landmark_visualization(Path("/Inter-Imges"), "texture", img)
     # 2. Find the one bounding box in the rendered image
     bboxes = detector(img)
     if len(bboxes) != 1:
@@ -79,9 +103,10 @@ def landmark_mesh(mesh, img_shape=(320, 240), verbose=False):
         groups = ['dlib_0', '__lsfm_on_surface']
         marker_edge_colours = ['blue', 'yellow']
 
+    # lm_img  : img after Land Mark
     lm_img = img.rasterize_landmarks(group=groups,
                                      line_colour='blue',
                                      marker_edge_colour=marker_edge_colours)
     return_dict['landmarked_image'] = lm_img
-
+    export_landmark_visualization(Path("/Inter-Imges"), "landmarked", lm_img)
     return return_dict
